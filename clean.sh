6f4dcac055c829120c7819a6819fdd5c1335bfdd
jay525 <jay.chuang525@gmail.com>
2017-09-18 00:34:25 +0800

    stop clean vig

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ find /home/ipopp/aquadaac/ -mtime +2 -print0 | xargs -0 -l1 -t rm -f
 find /home/ipopp/drl/data/pub/gsfcdata/aqua/modis/level0/ -mtime +2 -print0 | xargs -0 -l1 -t rm -f
 find /home/ipopp/drl/data/pub/gsfcdata/aqua/modis/level1/ -mtime +2 -print0 | xargs -0 -l1 -t rm -f
 find /home/ipopp/drl/data/pub/gsfcdata/aqua/modis/level2/ -mtime +2 -print0 | xargs -0 -l1 -t rm -f
-find /home/ipopp/gdfAQUA/ -mtime +2 -print0 | xargs -0 -l1 -t rm -f
+#find /home/ipopp/gdfAQUA/ -mtime +2 -print0 | xargs -0 -l1 -t rm -f
 
 #find /poes/data/hrptimages -mtime +15 -print0 | xargs -0 -l1 -t rm -f
 #find /poes/data/hrptimages -mtime +1 | find /poes/data/hrptimages -mtime +15 -print0 | xargs -0 -l1 -t rm -f
